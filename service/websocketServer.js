8042f8c34cf96ba39734542c46676109b4846c72
hk <Xiaojin@1017>
2024-07-16 10:00:23 +0800

    8080

--- service/websocketServer.js
+++ service/websocketServer.js
@@ -58,8 +58,8 @@ function broadcastOnlineUsers() {
 }
 
 // 监听服务器的端口，启动服务器 8080;
-server.listen(process.env.PORT, () => {
-  console.log('Server is listening on :', process.env.PORT);
+server.listen(8080, () => {
+  console.log('Server is listening on : 8080');
 });
 
 module.exports = server; 
