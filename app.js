ed6338235b2a760b43532b631d361fc26f854906
hk <Xiaojin@1017>
2024-07-16 11:13:16 +0800

    合并socket和appjs

--- app.js
+++ app.js
@@ -7,13 +7,73 @@ const logger = require('morgan');
 const http = require('http');
 const mongoose = require('mongoose');
 const trackUser = require('./middleware/trackUser');
-const { wss } = require('./service/websocketServer'); // 导入在线用户服务
+
+const { Server } = require('socket.io');
+const User = require('./models/User'); // 导入用户模型
+// const { wss } = require('./service/websocketServer'); // 导入在线用户服务
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 
 const app = express();
 const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "*", // 允许所有来源的跨域请求，根据实际需求设置
+    methods: ["GET", "POST"]
+  }
+});
+
+// 存储当前在线用户的信息，以用户标识符为键，用户信息为值
+const onlineUserInfos = new Map();
+
+// 处理客户端连接
+io.on('connection', (socket) => {
+  console.log('a user connected'); // 打印日志，表示有用户连接
+
+  // 监听客户端发送的页面浏览信息
+  socket.on('pageView', async (data) => {
+    const { fingerprint, ip, location, path, timestamp } = data;
+    const userInfo = await User.findOne({ fingerprint }); // 根据指纹查找用户信息
+
+    if (userInfo) {
+      // 更新用户信息
+      userInfo.ip = ip;
+      userInfo.location = location;
+      userInfo.path = path;
+      userInfo.timestamp = timestamp;
+      userInfo.socketId = socket.id; // 记录用户的Socket ID
+      onlineUserInfos.set(fingerprint, userInfo); // 将用户信息存储在 Map 中
+      broadcastOnlineUsers(); // 广播更新所有在线用户信息给所有客户端
+    } else {
+      socket.emit('error', { error: 'User not found' }); // 发送用户未找到的错误信息给客户端
+    }
+  });
+
+  // 监听客户端断开连接事件
+  socket.on('disconnect', () => {
+    // 当连接断开时，从在线用户信息 Map 中移除该用户信息
+    onlineUserInfos.forEach((userInfo, fingerprint) => {
+      if (userInfo.socketId === socket.id) {
+        onlineUserInfos.delete(fingerprint);
+        broadcastOnlineUsers(); // 广播更新所有在线用户信息给所有客户端
+      }
+    });
+    console.log('user disconnected'); // 打印日志，表示用户断开连接
+  });
+});
+
+// 广播所有在线用户信息给所有客户端
+function broadcastOnlineUsers() {
+  const onlineUsersArray = Array.from(onlineUserInfos.values());
+  io.emit('onlineUsers', { users: onlineUsersArray, count: onlineUsersArray.length });
+}
+
+// 监听服务器的端口，启动服务器 8080;
+server.listen(8080, () => {
+  console.log('Server is listening on : 8080');
+});
 
 
 // MongoDB 连接
@@ -49,11 +109,11 @@ app.use('/users', usersRouter);
 app.use('/api', trackUser); // 将中间件应用到 /api 路径下
 
 // 将 WebSocket 服务器与现有的 HTTP 服务器关联
-server.on('upgrade', function upgrade(request, socket, head) {
-  wss.handleUpgrade(request, socket, head, function done(ws) {
-    wss.emit('connection', ws, request);
-  });
-});
+// server.on('upgrade', function upgrade(request, socket, head) {
+//   wss.handleUpgrade(request, socket, head, function done(ws) {
+//     wss.emit('connection', ws, request);
+//   });
+// });
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
